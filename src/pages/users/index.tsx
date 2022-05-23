2eb72c5540c89a36ab378fd76bbff53dc0035712
Matheus <matheus.augustoreal@gmail.com>
2022-05-23 05:43:58 -0300

    Config React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,22 +1,24 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination/Index";
 import { Sidebar } from "../../components/Sidebar";
+import { useQuery } from "react-query";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    return data
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
 
   return (
     <Box>
@@ -43,7 +45,17 @@ export default function UserList() {
 
           </Flex>
 
-          <Table>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table>
             <Thead>
               <Tr>
                 <Th px={["4", "4", "6"]} color="gray.300" width="8" >
@@ -134,6 +146,9 @@ export default function UserList() {
           </Table>
 
           <Pagination />
+            </>
+          )}
+
         </Box>
       </Flex>
     </Box>
